4db9d6293c854aa4df2ab2da6b81cc23123f91be
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 20:34:42 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(collection)
   i = 0
   new_arr = []
   while i >= collection.length
-  new_arr << collection.map { |element| element }
+  new_arr << collection.collect { |element| element }
     i += 1
   end
   new_arr
